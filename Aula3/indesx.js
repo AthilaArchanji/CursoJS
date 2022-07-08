d2f8b96952152dcbf2caeee9a867d023a341e27b
Athila Archanji <athila.rodriguess@gmail.com>
2022-07-08 11:36:13 -0300

    Initial commit

---
+++ Aula3/indesx.js
@@ -0,0 +1,7 @@
+console.log('Athila "Arhanji"')
+console.log("Athila 'Arhanji'")
+console.log(`'Athila' "Arhanji"`)
+
+console.log(23, 5.03, "Athila de novo")
+
+//Em js não temos difereça de tipo entre inteiros e floats, os dois são do tipo number
