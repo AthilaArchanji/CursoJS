e2c10ef980d2d4aa3289eb2df5c48c6f8b75d51f
Athila Archanji <athila.rodriguess@gmail.com>
2022-07-09 18:39:17 -0300

    Estudando jss

---
+++ Aula7/index.js
@@ -0,0 +1,5 @@
+//para declar constantes usamos o conts
+const primeiroNumero = 3
+console.log(primeiroNumero)
+//não podemos mudar o valor de constantes
+//caso queira mudar o valor usar let em vez de constante
---
+++ Aula8/exercício.js
@@ -0,0 +1,14 @@
+const nome = 'Athila' 
+const sobrenome = 'Archanji Rodrigues' 
+const peso = 47
+const altura = 1.60
+const idade = 20
+let imc
+
+imc = peso / (altura* altura)
+
+let anoNascimento = 2022 - idade
+
+console.log(`${nome} ${sobrenome} tem ${peso} kg ${altura} de metros de altura e ${idade} de idade`)
+console.log(`seu imc é: ${imc}`)
+
