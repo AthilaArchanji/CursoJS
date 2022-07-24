1c1b7414784101a665bd922fbbdbc035c8c6dfde
Athila Archanji <athila.rodriguess@gmail.com>
2022-07-24 17:19:57 -0300

    Final exercicio

--- Aula30/Modelo/assets/js/main.js
+++ Aula30/Modelo/assets/js/main.js
@@ -1,3 +1,4 @@
+/* 
 let resultado = document.querySelector('.resultado')
 
 const data =  new Date("Octuber 7, 2019 22:52:00")
@@ -7,64 +8,55 @@ let mes = data.getMonth()
 let diaSemanaTexto
 let mesTexto
 
-switch(diaSemana){
-    case 0:
-        diaSemanaTexto = 'Domingo'
-        break
-    case 1:
-        diaSemanaTexto = 'Segunda-feira'
-        break
-    case 2:
-        diaSemanaTexto = 'Terça'
-        break
-    case 3:
-        diaSemanaTexto = 'Quarta'
-        break
-    case 4:
-        diaSemanaTexto = 'Quinta'
-        break
-    case 5:
-        diaSemanaTexto = 'Sexta'
-        break
-    case 6:
-        diaSemanaTexto = 'Sabado'
-        break
-    default:
-        diaSemanaTexto = ''
-        break
+resultado.innerHTML = `<p><b>${retornaDiaSemana(diaSemana)}, ${data.getDate()} de ${retornaMes(mes)} ${data.getFullYear()} ${data.getHours()}:${data.getMinutes()}</b></p>`
+
+function retornaDiaSemana(diaSemana){
+    switch(diaSemana){
+        case 0:
+            return diaSemanaTexto = 'Domingo'
+        case 1:
+            return diaSemanaTexto = 'Segunda-feira'
+        case 2:
+            return diaSemanaTexto = 'Terça'
+        case 3:
+            return diaSemanaTexto = 'Quarta'
+        case 4:
+            return diaSemanaTexto = 'Quinta'
+        case 5:
+            return diaSemanaTexto = 'Sexta'
+        case 6:
+            return diaSemanaTexto = 'Sabado'
+        default:
+            return diaSemanaTexto = ''
+    }
 }
 
-switch(mes){
-    case 0:
-        diaSemanaTexto = 'janeiro'
-        break
-    case 1:
-        diaSemanaTexto = 'fevereiro'
-        break
-    case 2:
-        diaSemanaTexto = 'abril'
-        break
-    case 3:
-        diaSemanaTexto = 'maio'
-        break
-    case 4:
-        diaSemanaTexto = 'junho'
-        break
-    case 5:
-        diaSemanaTexto = 'julho'
-        break
-    case 7:
-        diaSemanaTexto = 'agosto'
-        break
-    case 8:
-        diaSemanaTexto = 'setembro'
-        break
-    case 9:
-        mesTexto = 'Outubro'
-        break
+function retornaMes(mes){
+    switch(mes){
+        case 0:
+            return mesTexto = 'janeiro'
+        case 1:
+            return mesTexto = 'fevereiro'
+        case 2:
+            return mesTexto = 'abril'
+        case 3:
+            return mesTexto = 'maio'
+        case 4:
+            return mesTexto = 'junho'
+        case 5:
+            return mesTexto = 'julho'
+        case 7:
+            return mesTexto = 'agosto'
+        case 8:
+            return mesTexto = 'setembro'
+        case 9:
+            return mesTexto = 'Outubro'
+    }
 }
+esse foi o jeito burro de fazer isso
+ */
 
-switch(mes){
-}
+let resultado = document.querySelector('.resultado')
+const data = new Date()
 
-resultado.innerHTML = `<p><b>${diaSemanaTexto}, ${data.getDate()} de ${mesTexto}</b></p>`
+resultado.innerHTML = data.toLocaleString('pt-br', {dateStyle: 'full', timeStyle: 'short'})
