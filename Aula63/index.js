e2e0d570d6f5239208557598c251417669c83171
Athila Archanji <82385857+AthilaArchanji@users.noreply.github.com>
2022-08-25 09:24:42 -0300

    Início módulo 5 JS :tada:

---
+++ Aula63/index.js
@@ -0,0 +1,6 @@
+const nomes = ['Maria', 'Athila', 'Ana', 'Joziane', 'Robson', 'Isaac', 'Enzo']
+
+//nomes.splice(indice, delete, elem1, elem2, elem3)
+
+nomes.splice(2, 1, 'Julia')
+console.log(nomes)
