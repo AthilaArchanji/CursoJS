64c3f27dc3c9529481be9134808bc9030bb92216
Athila Archanji <athila.rodriguess@gmail.com>
2022-08-26 11:52:27 -0300

    avançando no módulo 6 do curso de JS

--- Aula70/index.js
+++ Aula70/index.js
@@ -1,7 +1,28 @@
-const pessoa = {
+/* const pessoa = {
     nome: 'athila',
     sobrenome: 'archanji'
 };
 
 const chave = 'nome'
-console.log(pessoa[chave]);
+//console.log(pessoa[chave]);
+
+const pessoa1 = new Object()
+const pessoa2 = new Object()
+
+pessoa1.nome = 'luis'
+pessoa1.sobrenome = 'sla'
+
+console.log(pessoa1)
+
+delete pessoa1.nome
+console.log(pessoa1) */
+
+function Pessoa(nome, sobrenome){
+    this.nome = nome
+    this.sobrenome = sobrenome
+}
+
+const p1 = new Pessoa('Athila', 'Archanji')
+const p2 = new Pessoa('Maria', 'Santos');
+
+console.log(p1)
---
+++ Aula71/index.js
@@ -0,0 +1,17 @@
+function Produto(nome, preco, estoque) {
+    this.nome = nome
+    this.preco = preco
+    this.estoque = estoque
+
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true, //moostra a chave
+        value: estoque, //valor
+        writable: true, //pode alterar o valor
+        configurable: false //configurável
+    })
+}
+
+const p1 = new Produto('camiseta', 20, 3)
+p1.estoque = 500000
+delete p1.estoque
+console.log(p1)
