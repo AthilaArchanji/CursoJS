0ddf8fd23f22f29dc736beef3adb16ec8fe7f01f
Athila <athila.rodriguess@gmail.com>
2022-11-08 19:44:09 -0300

    Aula 9 finalizada :rocket:

--- NODE/Aula9/controllers/homeController.js
+++ NODE/Aula9/controllers/homeController.js
@@ -1,3 +1,7 @@
 exports.paginaInicial = (req, res) => {
     res.send('<form action="/" method="POST"> Nome do cliente <input type="text" name="nome"> <button>Ola caralho </button> </form>');
 }
+
+exports.trataPost = (req, res) => {
+    res.send('Ei, sou sua nova rota de post!!!')
+}
--- NODE/Aula9/server.js
+++ NODE/Aula9/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const routes = require('/routes.js')
+const routes = require('./routes')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(routes)
@@ -8,22 +8,4 @@ app.use(routes)
 app.listen(3000, () => {
     console.log('Acessar http://localhost:3000')
     console.log('servidor executanto na porta 3000')
-})
-
-app.get('/', (req, res) => {
-
-});
-
-app.get('/contato', (req, res) => {
-    res.send(req.params.idUsuario)
-})
-
-app.get('/testes/:idUsuario?', (req, res) => {
-    console.log(req.query)
-    res.send('sla')
-})
-
-app.post('/', (req, res) => {
-    console.log(req.body)
-    res.send(`${req.body.nome}`)
 })
